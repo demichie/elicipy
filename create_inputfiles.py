39abf91d56781c7c0372353864ce37b980f95e35
Mattia de' Michieli Vitturi <demichie@gmail.com>
2022-06-15 10:58:55 +0200

    Create create_inputfiles.py
    new script to create input folders for simulations from values in the csv file

---
+++ create_inputfiles.py
@@ -0,0 +1,75 @@
+import pandas as pd
+import numpy as np
+import os
+import shutil as st
+from ElicipyDict import *
+
+
+def replace_strings(working_dir, df , header , i_row):
+
+    main_dir = os.getcwd()
+    os.chdir(working_dir)
+    directory = os.getcwd()
+
+    for fname in os.listdir(directory):
+
+        if os.path.isfile(fname):
+            # print(fname)
+            # Full path
+            f = open(fname, 'r')
+            filedata = f.read()
+
+            for name in header:
+
+                searchstring = name
+                # print(searchstring)
+                f.seek(0)
+
+                if searchstring in filedata:
+
+                    print('found string in file %s' % fname)
+                    print(searchstring)
+                    f.seek(0)
+
+                    for line in f:
+                        # replacing the string and write to output file
+                        filedata = filedata.replace(searchstring,
+                                                    str(df.at[i_row, name]))
+
+            f.close()
+            f_out = open(fname, 'w')
+            f_out.write(filedata)
+            f_out.close()
+
+    os.chdir(main_dir)
+
+##########################################
+
+def main():
+
+    csv_file = output_dir + '/' + elicitation_name + '_samples.csv'
+
+    df = pd.read_csv(csv_file)
+
+    templatedir = 'templatedir'
+
+    print(df)
+
+    header = df.columns
+
+    print(header)
+
+    n_rows = len(df.index)
+    for i_row in range(n_rows):
+
+        working_dir = "ensemble." + "{:05d}".format(i_row)
+        working_dir = os.path.join(os.getcwd(), working_dir)
+    
+        st.copytree('templatedir', working_dir, symlinks=True)
+    
+        replace_strings(working_dir, df , header , i_row)
+        
+
+if __name__ == '__main__':
+
+    main()
