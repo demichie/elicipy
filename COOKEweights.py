ebf695badfbb65ebd1605c928fd02e2f3dfe216b
Mattia de' Michieli Vitturi <demichie@gmail.com>
2023-03-02 09:26:13 +0100

    Update COOKEweights.py
    fixed code formatting

--- COOKEweights.py
+++ COOKEweights.py
@@ -1,8 +1,7 @@
 def COOKEweights(SQ_array, TQ_array, realization, alpha, background_measure,
-                   overshoot, cal_power):
-
+                 overshoot, cal_power):
     """Compute the weights with Cooke formulation
-    
+
     Parameters
     ----------
     SQ_array : numpy array
@@ -17,20 +16,20 @@ def COOKEweights(SQ_array, TQ_array, realization, alpha, background_measure,
         Python list
     overshoot : float
     cal_power : float
-    
+
     Returns
     -------
     W : numpy array
         an array with weights
-         
-    This function is based on the Matlab package 
-    ANDURIL (Authors:  Georgios Leontaris and 
+
+    This function is based on the Matlab package
+    ANDURIL (Authors:  Georgios Leontaris and
     Oswaldo Morales-Napoles)
     """
 
     import numpy as np
 
-    # number of Seed questions    
+    # number of Seed questions
     N = SQ_array.shape[2]
     # number of experts
     E = SQ_array.shape[0]
@@ -40,7 +39,7 @@ def COOKEweights(SQ_array, TQ_array, realization, alpha, background_measure,
     C = np.zeros((E))
     # unNormalized weight
     w = np.zeros((E))
-    # array with information, score and normalized weight for each expert 
+    # array with information, score and normalized weight for each expert
     W = np.zeros((E, 5))
 
     # create numpy array M with the number of realizations captured in every
@@ -48,116 +47,114 @@ def COOKEweights(SQ_array, TQ_array, realization, alpha, background_measure,
     for ex in np.arange(E):
         for i in np.arange(N):
             if realization[i] < SQ_array[ex, 0, i]:
-            
+
                 M[ex, 0] = M[ex, 0] + 1
-                
+
             elif realization[i] == SQ_array[ex, 0, i]:
-            
+
                 M[ex, 0] = M[ex, 0] + 0.5
                 M[ex, 1] = M[ex, 1] + 0.5
-                
+
             elif realization[i] < SQ_array[ex, 1, i]:
-            
+
                 M[ex, 1] = M[ex, 1] + 1
-                
+
             elif realization[i] == SQ_array[ex, 1, i]:
-            
+
                 M[ex, 1] = M[ex, 1] + 0.5
                 M[ex, 2] = M[ex, 2] + 0.5
-                
+
             elif realization[i] < SQ_array[ex, 2, i]:
-            
+
                 M[ex, 2] = M[ex, 2] + 1
-                
+
             elif realization[i] == SQ_array[ex, 2, i]:
-            
+
                 M[ex, 2] = M[ex, 2] + 0.5
                 M[ex, 3] = M[ex, 3] + 0.5
-                
+
             else:
-            
+
                 M[ex, 3] = M[ex, 3] + 1
 
     # calculate calibration and information score for every expert
-    [I_real, I_tot] = calculate_information(SQ_array, TQ_array, realization, overshoot,
-                                            background_measure)
-    
+    [I_real, I_tot] = calculate_information(SQ_array, TQ_array, realization,
+                                            overshoot, background_measure)
+
     for ex in np.arange(E):
-    
+
         C[ex] = calscore(M[ex, :], cal_power)
-        
+
         W[ex, 0] = C[ex]
         W[ex, 1] = I_tot[ex]
         W[ex, 2] = I_real[ex]
 
         w[ex] = C[ex] * I_real[ex]
-        
-   
-                   
+
     # unNormalized weight
     W[:, 3] = w.T
 
     # set to zero based on calibration score
     w[C < alpha] = 0.0
-    
+
     # set to zero based on weight
     # w[ (w/np.sum(w)) < alpha] = 0.0
 
-    
     # Normalized weight
     W[:, 4] = w / np.sum(w)
 
-    return W,C,I_real
+    return W, C, I_real
+
 
 def calscore(M, cal_power):
-
     """Compute single expert score
-    
+
     Parameters
     ----------
     M : numpy array
-        Number of realizations captured in every expert's bin 
+        Number of realizations captured in every expert's bin
         that is formed by the provided quantiles
     cal_power : float
-    
+
     Returns
     -------
     CS : float
          Score
-    
-    This function is based on the Matlab package 
-    ANDURIL (Authors:  Georgios Leontaris and 
+
+    This function is based on the Matlab package
+    ANDURIL (Authors:  Georgios Leontaris and
     Oswaldo Morales-Napoles)
     """
 
     import numpy as np
     from scipy.stats import chi2
-    
+
     N = np.sum(M)  # number of seed items
     S = M / N
 
     if np.sum(np.isnan(S)) == len(S):
-    
+
         CS = np.nan
 
     else:
         if len(S) == 4:
             P = np.array([0.05, 0.45, 0.45, 0.05])
         elif length(S) == 6:
-            P = np.array([0.05, 0.20, 0.25, 0.25, 0.20, 0.05]) 
+            P = np.array([0.05, 0.20, 0.25, 0.25, 0.20, 0.05])
 
         E1 = S * np.log(S / P)
         MI = np.sum(E1[~np.isnan(E1)])
         E = 2.0 * N * MI * cal_power
         CS = chi2.sf(E, len(S) - 1)
-        
+
     return CS
 
-def calculate_information(SQ_array, TQ_array, realization, k, background_measure):
 
+def calculate_information(SQ_array, TQ_array, realization, k,
+                          background_measure):
     """
-    This function is based on the Matlab package 
-    ANDURIL (Authors:  Georgios Leontaris and 
+    This function is based on the Matlab package
+    ANDURIL (Authors:  Georgios Leontaris and
     Oswaldo Morales-Napoles)
     """
 
@@ -173,8 +170,8 @@ def calculate_information(SQ_array, TQ_array, realization, k, background_measure
 
     Nq = TQ_array.shape[2]
 
-    l = np.zeros(N + Nq)
-    h = np.zeros(N + Nq)
+    lowval = np.zeros(N + Nq)
+    highval = np.zeros(N + Nq)
 
     x_o = np.zeros(N + Nq)
     x_n = np.zeros(N + Nq)
@@ -184,16 +181,16 @@ def calculate_information(SQ_array, TQ_array, realization, k, background_measure
     Info_score_tot = np.zeros((E))
 
     for i in np.arange(N):
-    
-        l[i] = np.minimum(np.amin(SQ_array[:, :, i]), realization[i])
-        h[i] = np.maximum(np.amax(SQ_array[:, :, i]), realization[i])
+
+        lowval[i] = np.minimum(np.amin(SQ_array[:, :, i]), realization[i])
+        highval[i] = np.maximum(np.amax(SQ_array[:, :, i]), realization[i])
 
         for e in np.arange(E):
 
             if background_measure[i] == 'uni':
 
-                x_o[i] = l[i] - k * (h[i] - l[i])
-                x_n[i] = h[i] + k * (h[i] - l[i])
+                x_o[i] = lowval[i] - k * (highval[i] - lowval[i])
+                x_n[i] = highval[i] + k * (highval[i] - lowval[i])
 
                 tmp = np.insert(SQ_array[e, :, i], 0, x_o[i])
                 tmp2 = np.append(tmp, x_n[i])
@@ -201,14 +198,17 @@ def calculate_information(SQ_array, TQ_array, realization, k, background_measure
 
             elif background_measure[i] == 'log':
 
-                if l[i] < 0 or h[i] < 0:
+                if lowval[i] < 0 or highval[i] < 0:
 
                     raise ValueError(
-                        'Log-Uniform background measure cannot be used for item %d because an expert provided negative values',
-                        i)
+                        'Log-Uniform background measure cannot be used '
+                        'for item %d because an expert provided '
+                        'negative values', i)
 
-                x_o[i] = np.log(l[i]) - k * (np.log(h[i]) - np.log(l[i]))
-                x_n[i] = np.log(h[i]) + k * (np.log(h[i]) - np.log(l[i]))
+                x_o[i] = np.log(lowval[i]) - k * \
+                    (np.log(highval[i]) - np.log(lowval[i]))
+                x_n[i] = np.log(highval[i]) + k * \
+                    (np.log(highval[i]) - np.log(lowval[i]))
                 tmp = np.insert(np.log(SQ_array[e, :, i]), 0, x_o[i])
                 tmp2 = np.append(tmp, x_n[i])
                 x[e, :] = tmp2
@@ -229,42 +229,46 @@ def calculate_information(SQ_array, TQ_array, realization, k, background_measure
     sumb = np.zeros((N_items_tot, E))
 
     for i in np.arange(SQ_array.shape[2], N_items_tot):
-    
+
         if not realization[i]:  # information of Target Questions
 
-            l[i] = np.amin(TQ_array[:, :, i - SQ_array.shape[2]])
-            h[i] = np.amax(TQ_array[:, :, i - SQ_array.shape[2]])
+            lowval[i] = np.amin(TQ_array[:, :, i - SQ_array.shape[2]])
+            highval[i] = np.amax(TQ_array[:, :, i - SQ_array.shape[2]])
 
             for e in np.arange(E):
-            
+
                 if background_measure[i] == 'uni':
-                
-                    x_o[i] = l[i] - k * (h[i] - l[i])
-                    x_n[i] = h[i] + k * (h[i] - l[i])
 
-                    tmp = np.insert(
-                        TQ_array[e, :, i - SQ_array.shape[2]], 0, x_o[i])
-                    tmp2 = np.append(tmp, x_n[i])
-                    x[e, :] = tmp2
+                    x_o[i] = lowval[i] - k * (highval[i] - lowval[i])
+                    x_n[i] = highval[i] + k * (highval[i] - lowval[i])
 
-                elif background_measure[i] == 'log':
-                
-                    if l(i) < 0 or h(i) < 0:
-                    
-                        raise ValueError(
-                            'Log-Uniform background measure cannot be used for item %d because an expert provided negative values',
-                            i)
-
-                    x_o[i] = np.log(l[i]) - k * (np.log(h[i]) - np.log(l[i]))
-                    x_n[i] = np.log(h(i)) + k * (np.log(h[i]) - np.log(l[i]))
-
-                    tmp = np.insert(np.log(TQ_array[e, :, i - SQ_array.shape[2]]), 0,
+                    tmp = np.insert(TQ_array[e, :, i - SQ_array.shape[2]], 0,
                                     x_o[i])
                     tmp2 = np.append(tmp, x_n[i])
                     x[e, :] = tmp2
 
+                elif background_measure[i] == 'log':
+
+                    if l(i) < 0 or h(i) < 0:
+
+                        raise ValueError(
+                            'Log-Uniform background measure cannot be used '
+                            'for item %d because an expert provided '
+                            'negative values', i)
+
+                    x_o[i] = np.log(lowval[i]) - k * \
+                        (np.log(highval[i]) - np.log(lowval[i]))
+                    x_n[i] = np.log(h(i)) + k * \
+                        (np.log(highval[i]) - np.log(lowval[i]))
+
+                    tmp = np.insert(
+                        np.log(TQ_array[e, :, i - SQ_array.shape[2]]), 0,
+                        x_o[i])
+                    tmp2 = np.append(tmp, x_n[i])
+                    x[e, :] = tmp2
+
                 for j in np.arange(N_P):
-                
+
                     sumb[i - SQ_array.shape[2],
                          e] = sumb[i - SQ_array.shape[2],
                                    e] + p[j] * np.log(p[j] /
@@ -281,6 +285,3 @@ def calculate_information(SQ_array, TQ_array, realization, k, background_measure
         Info_score_tot[e] = np.sum(info_per_variable[:, e]) / N_items_tot
 
     return Info_score_real, Info_score_tot
-
-    
-
