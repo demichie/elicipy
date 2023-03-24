d66cfd007aad27c406ce6213d03d24488e9081cb
Mattia de' Michieli Vitturi <demichie@gmail.com>
2023-03-24 15:39:00 +0100

    added case
    added Cotopaxi  elicitation

---
+++ ELICITATIONS/Cotopaxi/ElicipyDict.py
@@ -0,0 +1,67 @@
+elicitation_name = 'test'
+
+# datarepo can be 'local' or 'github'
+datarepo = 'local'
+
+# if datarepo is 'github' you need user and token to access it
+# user = ''
+# github_token = ''
+
+# folder or repository with data
+Repository = 'testWebform'
+
+output_dir = 'OUTPUT'
+
+# comment if there is ony one language
+language = 'ENG'
+
+analysis = True
+target = True
+postprocessing = True
+
+# target_list = [2,3]
+# seed_list = [1,2]
+
+# group_list = [5,6]
+
+n_sample = 5000
+n_bins = 10
+
+# hist_type: 'bar' or 'step'
+hist_type = 'bar'
+
+# EW_flag (equal weights):
+# 0 - no EW
+# 1 - EW 
+EW_flag = 1
+
+# ERF_flag: 
+# 0 - no ERF
+# 1 - ERF original 
+# 2 - ERF modified
+ERF_flag = 1
+
+# 
+Cooke_flag = 0
+
+# parameters for Cooke
+
+# significance level (this value cannot be higher than the
+# highest calibration score of the pool of experts)
+alpha = 0.05  
+
+# overshoot for intrinsic range
+overshoot = 0.1  
+
+# global cal_power
+# this value should be between [0.1, 1]. The default is 1.
+cal_power = 1
+
+# tree parameters
+first_node_list = [1]
+first_node_str_list = ['']
+
+# groups for trend plots
+# trend_groups = [ [2,3] ]
+
+
---
+++ ELICITATIONS/Cotopaxi/createWebformDict.py
@@ -0,0 +1,41 @@
+# select 'github' or 'local'
+datarepo = 'local'
+
+# datarepo = 'github'
+# RepositoryData = 'createWebformAnswers'
+
+Repository = 'testWebform'
+
+# this can be 'seed' or 'target'
+quest_type = 'target'
+
+group_list = ['SG1 (Clermont)','SG2 (Quito)','SG3 (Geol.)','SG4 (MathMod)','SG5 (Junior)','SG6 (Senior)']
+
+# target_list = [2,3]
+# seed_list = [1,2]
+absolute_indexing = True
+
+
+# encrypted = False
+
+# user = 'username'
+# github_token = "token"
+
+pctls = [5, 50, 95]
+# companion_document = ""
+# supplementary_documents = [""]
+
+confirmation_email = False
+
+# if confirmation email is True and datarepo is 'local' 
+# fill the following fields, if 'github' add to streamlit
+# secrets
+
+# SENDER_ADDRESS
+# SENDER_PASSWORD
+# SENDER_NAME
+# SMTP_SERVER_ADDRESS
+# PORT
+
+
+
