e34d2d27b8645490b5ca5bbdca634158bfcc5e04
Alessandro Tadini <48525177+aletadini@users.noreply.github.com>
2024-03-08 10:46:22 +0100

    Add files via upload

--- UTILS/dtt_to_csv.py
+++ UTILS/dtt_to_csv.py
@@ -3,15 +3,28 @@ import csv
 import numpy as np
 import time
 from datetime import datetime
-import names
+import os
+import os.path
 
-nome_file = 'seed.rls'
+
+folder = "DTT_TO_CSV"
+
+if os.path.isdir(folder):
+    pass
+else:
+    os.mkdir(folder)
+    os.mkdir(os.path.join(os.getcwd(), folder, "seed"))
+    os.mkdir(os.path.join(os.getcwd(), folder, "target"))
+
+# -------
+
+nome_file = 'Vesuvio_2015_ok.rls'
 
 # Lista per i valori della quarta colonna
 realization = []
 
 # Apri il file in modalità lettura
-with open(nome_file, 'r') as file:
+with open(nome_file, 'r', encoding='latin-1') as file:
     # Leggi il contenuto del file riga per riga
     for riga in file:
         # Dividi la riga in colonne utilizzando uno spazio come delimitatore
@@ -26,13 +39,14 @@ with open(nome_file, 'r') as file:
 print('realizations', realization)
 
 # Nome del file della tabella
-nome_file = 'seed_and_target.dtt'
+nome_file = 'Vesuvio_2015_ok.dtt'
 
 # Numero di colonne della tabella
 num_colonne = 8
 
 # Apri il file in modalità lettura
-with open(nome_file, 'r') as file:
+with open(nome_file, 'r', encoding='latin-1') as file:
+
     # Leggi la prima riga
     prima_riga = file.readline().strip().split()
 
@@ -42,52 +56,74 @@ with open(nome_file, 'r') as file:
     # Definisci la lista di percentili
     percentiles = [f'{num}%ile' for num in numeri_percentili]
 
-    print(percentiles)
+    print('percentiles', percentiles)
 
-    # Salta le prime 4 righe
-    for _ in range(3):
+    # Salta la prima riga
+    for _ in range(1):
         next(file)
 
 # Apri il file in modalità lettura
-with open(nome_file, 'r') as file:
+with open(nome_file, 'r', encoding='latin-1') as file:
     # Crea un lettore CSV
     lettore_csv = csv.reader(file)
 
-    # Leggi le prime 4 righe come header
+    # Leggi la prima riga come header, poi leggi fino all'ultima domanda del
+    # primo esperto per ottenere il testo delle domande e i nomi - mod AT
     header = []
-    for _ in range(3):
+    names_f = []
+    qst_txt_f = []
+    dati = []
+
+    for _ in range(1):
         riga = next(lettore_csv)
         header.append(riga)
+    for _ in range(1):
+        riga = next(lettore_csv)
+        dati.append(riga[0].split()[:8])
+        names_f.append(riga[0].split()[-2:])
+        qst_txt_f.append(riga[0].split()[8:-2])
+        txt_cn1 = ' '.join(riga[0].split()[8:-2])
+        txt_cn1bis = riga[0].split()[8:-2]
+        len_cn1bis = len(txt_cn1bis)
 
-    # Leggi il resto dei dati
-    dati = []
     for riga in lettore_csv:
-        # print(riga)
-        dati.append(riga[0].split())
+        txt_cnn = ' '.join(riga[0].split()[8:])
+        txt_cnnbis = riga[0].split()[8:]
+        if len(txt_cnnbis) > 0 and txt_cnnbis[0] == txt_cn1bis[
+                0] and txt_cnnbis[1] == txt_cn1bis[1]:
+            n_int = riga[0].split()[8:]
+            names_f.append(n_int[len_cn1bis:])
+            dati.append(riga[0].split()[:8])
+        elif len(riga[0].split()) > 8:
+            dati.append(riga[0].split()[:8])
+            qst_txt_f.append(riga[0].split()[8:])
+        else:
+            dati.append(riga[0].split()[:8])
+
+names = []
+qst_txt = []
+for j in names_f:
+    n = ' '.join(j)
+    names.append(n)
+for j in qst_txt_f:
+    n = ' '.join(j)
+    qst_txt.append(n)
+# print(qst_txt)
 
 header = ["idx_expert", "expert_name", "idx_q", "type_and_idx_q", "scale"
           ] + percentiles
 
-# print(header)
+print(header)
 # Crea un DataFrame utilizzando l'header e i dati
 df = pd.DataFrame(data=dati, columns=header)
+df = df.replace('-9.99500E+0002', '')
+df = df.replace('-9.99600E+0002', '')
 
 # Stampa il DataFrame
 print(df)
 
 idx_experts = np.unique(np.array(df['idx_expert'], dtype=int))
-print(idx_experts)
 n_experts = idx_experts.shape[0]
-
-rand_first_name = []
-rand_last_name = []
-
-for i in range(n_experts):
-    first_name = names.get_first_name()
-    rand_first_name.append(first_name)
-    last_name = names.get_last_name()
-    rand_last_name.append(last_name)
-    print(first_name + ',' + last_name)
 
 header_quest = [
     'IDX', 'LABEL', 'SHORT Q', 'LONG Q_ENG', 'UNITS', 'SCALE', 'MINVAL',
@@ -98,7 +134,7 @@ data_quest = []
 
 for count, idx in enumerate(idx_experts):
 
-    print('SQ', idx)
+    print('Seed Questions for Exp', idx)
     columns = []
     data = []
     rslt_df = df[df['idx_expert'] == str(idx)]
@@ -106,26 +142,31 @@ for count, idx in enumerate(idx_experts):
     name = list(rslt_df['expert_name'])[0]
 
     columns.append('First Name')
-    data.append(rand_first_name[count])
+    data.append(names[count].rsplit(' ', 1)[0])
 
     columns.append('Last Name')
-    data.append(rand_last_name[count])
+    data.append(names[count].rsplit(' ', 1)[1])
 
     columns.append('Email address')
-    data.append(rand_last_name[count] + '@mail.com')
+    data.append(names[count].rsplit(' ', 1)[0] +
+                names[count].rsplit(' ', 1)[1] + '@mail.com')
 
     columns.append('Group(s)')
     data.append('0')
+    print(names[count].rsplit(' ', 1)[1])
 
     n_SQ = 0
+    k = 0
 
     for index, row in rslt_df.iterrows():
 
         row_sq = []
 
-        if 'SQ' in row['type_and_idx_q']:
+        if realization[k] != -999.5 and realization[k] != -999.6:
             n_SQ += 1
-            seed_idx = int(str(row['type_and_idx_q']).replace('SQ', ''))
+            k += 1
+            # seed_idx = re.findall(r'\d+', row['type_and_idx_q'])[0]
+            seed_idx = k
             columns.append(row['type_and_idx_q'] + ' - 5%ile (0;inf) []')
             data.append(row['5%ile'])
             columns.append(row['type_and_idx_q'] + ' - 50%ile (0;inf) []')
@@ -134,16 +175,15 @@ for count, idx in enumerate(idx_experts):
             data.append(row['95%ile'])
 
             if idx == 1:
-
-                row_sq.append(int(row['type_and_idx_q'].replace('SQ', '')))
-                row_sq.append(str(row['type_and_idx_q']))
+                row_sq.append(seed_idx)
                 row_sq.append(row['type_and_idx_q'])
-                row_sq.append('Long text')
+                row_sq.append(row['type_and_idx_q'])
+                row_sq.append(qst_txt[index])
                 row_sq.append('[]')
-                row_sq.append(str(row['scale']))
+                row_sq.append(str(row['scale']).lower())
                 row_sq.append(0)
                 row_sq.append('inf')
-                row_sq.append(str(realization[seed_idx - 1]))
+                row_sq.append(str(realization[int(seed_idx) - 1]))
                 row_sq.append('seed')
                 row_sq.append(0)
                 row_sq.append(0)
@@ -152,22 +192,41 @@ for count, idx in enumerate(idx_experts):
                 row_sq.append('')
 
                 data_quest.append(row_sq)
+        else:
+            k += 1
 
     now = datetime.now()
     dt_string = now.strftime("%Y_%m_%d_%H_%M_%S")
-    new_file = './DTT_TO_CSV/seed/questionnaire_' + dt_string + '_Output.csv'
 
     # Write the merged data to a new CSV file
-    with open(new_file, 'w', newline='') as outfile:
-        writer = csv.writer(outfile)
-        writer.writerow(columns)  # Write the updated header row
-        writer.writerow(data)  # Write the merged data row
+    if all(data) is True:
+        new_filename = './DTT_TO_CSV/seed/questionnaire_' + \
+                       dt_string + '_Output.csv'
+        with open(new_filename, 'w', newline='') as outfile:
+            writer = csv.writer(outfile)
+            writer.writerow(columns)  # Write the updated header row
+            writer.writerow(data)  # Write the merged data row
+    else:
+        print(
+            "Warning: Exp %s has not given answers to one or more seed, \
+             saving his/her answers in a separate folder"
+            % names[count].rsplit(' ', 1)[1])
+        if os.path.isdir('./DTT_TO_CSV/seed_missing'):
+            pass
+        else:
+            os.mkdir('./DTT_TO_CSV/seed_missing')
+        new_filename = './DTT_TO_CSV/seed_missing/questionnaire_' + \
+                       dt_string + '_Output.csv'
+        with open(new_filename, 'w', newline='') as outfile:
+            writer = csv.writer(outfile)
+            writer.writerow(columns)  # Write the updated header row
+            writer.writerow(data)  # Write the merged data row
 
     time.sleep(2)
 
 for count, idx in enumerate(idx_experts):
 
-    print('TQ', idx)
+    print('Target Questions for Exp', idx)
     columns = []
     data = []
     rslt_df = df[df['idx_expert'] == str(idx)]
@@ -175,25 +234,31 @@ for count, idx in enumerate(idx_experts):
     name = list(rslt_df['expert_name'])[0]
 
     columns.append('First Name')
-    data.append(rand_first_name[count])
+    data.append(names[count].rsplit(' ', 1)[0])
 
     columns.append('Last Name')
-    data.append(rand_last_name[count])
+    data.append(names[count].rsplit(' ', 1)[1])
 
     columns.append('Email address')
-    data.append(rand_last_name[count] + '@mail.com')
+    data.append(names[count].rsplit(' ', 1)[0] +
+                names[count].rsplit(' ', 1)[1] + '@mail.com')
 
     columns.append('Group(s)')
     data.append('0')
+    print(names[count].rsplit(' ', 1)[1])
 
     n_TQ = 0
+    k = 0
 
     for index, row in rslt_df.iterrows():
 
         row_tq = []
 
-        if 'TQ' in row['type_and_idx_q']:
+        if realization[k] == -999.5 or realization[k] == -999.6:
             n_TQ += 1
+            k += 1
+            # target_idx = re.findall(r'^\D*(\d+)', row['type_and_idx_q'])[0]
+            target_idx = k
             columns.append(row['type_and_idx_q'] + ' - 5%ile (0;inf) []')
             data.append(row['5%ile'])
             columns.append(row['type_and_idx_q'] + ' - 50%ile (0;inf) []')
@@ -202,16 +267,15 @@ for count, idx in enumerate(idx_experts):
             data.append(row['95%ile'])
 
             if idx == 1:
-
-                row_tq.append(int(row['type_and_idx_q'].replace('TQ', '')))
-                row_tq.append(str(row['type_and_idx_q']))
+                row_tq.append(n_TQ)
                 row_tq.append(row['type_and_idx_q'])
-                row_tq.append('Long text')
+                row_tq.append(row['type_and_idx_q'])
+                row_tq.append(qst_txt[index])
                 row_tq.append('[]')
-                row_tq.append(str(row['scale']))
+                row_tq.append(str(row['scale']).lower())
                 row_tq.append(0)
                 row_tq.append('inf')
-                row_tq.append(str(realization[seed_idx - 1]))
+                row_tq.append(' ')
                 row_tq.append('target')
                 row_tq.append(0)
                 row_tq.append(0)
@@ -220,20 +284,39 @@ for count, idx in enumerate(idx_experts):
                 row_tq.append('')
 
                 data_quest.append(row_tq)
+        else:
+            k += 1
 
     now = datetime.now()
     dt_string = now.strftime("%Y_%m_%d_%H_%M_%S")
-    new_file = './DTT_TO_CSV/target/questionnaire_' + dt_string + '_Output.csv'
 
     # Write the merged data to a new CSV file
-    with open(new_file, 'w', newline='') as outfile:
-        writer = csv.writer(outfile)
-        writer.writerow(columns)  # Write the updated header row
-        writer.writerow(data)  # Write the merged data row
+    if all(data) is True:
+        new_filename = './DTT_TO_CSV/target/questionnaire_' + dt_string \
+                      + '_Output.csv'
+        with open(new_filename, 'w', newline='') as outfile:
+            writer = csv.writer(outfile)
+            writer.writerow(columns)  # Write the updated header row
+            writer.writerow(data)  # Write the merged data row
+    else:
+        print(
+            "Warning: Exp %s has not given answers to one or more target, \
+             saving his/her answers in a separate folder"
+            % names[count].rsplit(' ', 1)[1])
+        if os.path.isdir('./DTT_TO_CSV/target_missing'):
+            pass
+        else:
+            os.mkdir('./DTT_TO_CSV/target_missing')
+        new_filename = './DTT_TO_CSV/target_missing/questionnaire_' + \
+                       dt_string + '_Output.csv'
+        with open(new_filename, 'w', newline='') as outfile:
+            writer = csv.writer(outfile)
+            writer.writerow(columns)  # Write the updated header row
+            writer.writerow(data)  # Write the merged data row
 
     time.sleep(2)
 
-new_file = './DTT_TO_CSV/questionnaire.csv'
+new_filename = './DTT_TO_CSV/questionnaire.csv'
 
 df = pd.DataFrame(data_quest, columns=header_quest)
-df.to_csv(new_file, index=False)
+df.to_csv(new_filename, index=False)
